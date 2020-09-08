e16b49c549919a0b08b6e0aa3aeeb94fff7e70fb
Klara Wichmann <klara@imagomat.de>
2020-09-08 17:09:48 +0200

    small changes in organisation of camera class - needed to commit in order to do git pull

--- source/folders/camera/camera.cpp
+++ source/folders/camera/camera.cpp
@@ -1,37 +1,46 @@
 #include "camera.hpp"
-#include <math.h>
 
-#include <utility>
+/**
+ Camera constructor with angle/ fov
+ @param name of the camera
+ @param fov field of view in degrees
+*/
 
-
-Camera::Camera(std::string  name, float fov_x) :
-    name_ {std::move(name)},
-    position_{0.0f, 0.0f, 0.0f},
-    direction_{glm::vec3{0.0000001, 0.00000001, -1}},
-    width_{1},
-    height_{1}, //TODO Berechnen durch fov in y Richtung?
-    distance_{fov_x}, // from presentation
+Camera::Camera (std::string const& name, float fov_x) :
+    name_ {name},
+    position_ {0.0f, 0.0f, 0.0f},
+    direction_ {0.0000001f, 0.00000001f, -1.0f},
+    width_ {1},
+    height_ {1}, //TODO Berechnen durch fov in y Richtung?
+    distance_ {fov_x},
     fov_x_ {fov_x}
 {
     distance_ *= MATH_PI / 360.0f;
     distance_ = (float) (0.5f / std::tan(distance_)); // from presentation
 }
 
-Camera::Camera (std::string  name, float fov_x, glm::vec3 const& eye, glm::vec3 const& direction, glm::vec3 const& up) :
-    name_ {std::move(name)},
-    position_ {eye}, //correct value?
+/**
+ Camera constructor with angle/ fov + position + direction + up-vector of camera
+ @param name of the camera
+ @param fov field of view in degrees
+ @param eye position of the camera as vec3
+ @param direction of the camera as vec3
+ @param up up-vector of the camera as vec3
+ */
+
+Camera::Camera (std::string const& name, float fov_x, glm::vec3 const& eye, glm::vec3 const& direction, glm::vec3 const& up) :
+    name_ {name},
+    position_ {eye},
     direction_ {direction},
     width_{1},
     height_{1}, //TODO Berechnen durch fov in y Richtung?
-    distance_{fov_x}, // from presentation
+    distance_{fov_x},
     up_vector_ {up},
     fov_x_ {fov_x}
 {
     distance_ *= MATH_PI / 360.0f;
     distance_ = (float) (0.5f / std::tan(distance_)); // from presentation
 }
-
-
 
 /**
  * Camera constructor without angle / fov
@@ -41,31 +50,34 @@ Camera::Camera (std::string  name, float fov_x, glm::vec3 const& eye, glm::vec3 
  * @param height of the screen in pixel/ int
  * @param distance of the cameraplane from the cameraorigin to the cameraplane
  */
-Camera::Camera(glm::vec3 const &position, glm::vec3 const &direction, int width, int height, float distance) :
-position_{position},
-direction_{direction},
-width_{width},
-height_{height},
-distance_{distance} {
+
+Camera::Camera (glm::vec3 const &position, glm::vec3 const &direction, int width, int height, float distance) :
+    position_ {position},
+    direction_ {direction},
+    width_ {width},
+    height_ {height},
+    distance_ {distance}
+{
     this->direction_ = glm::normalize(this->direction_);
 }
 
 /**
  *
- * Camera constructor with fov
- * Looks at 0, 0, -1
+ * Camera constructor with fov - looks at 0, 0, -1
  * @param position of the camera as vec3
  * @param direction of the camera as vec3
  * @param width of the screen in pixel/ int
  * @param height of the screen in pixel/ int
  * @param fov field of view in degrees
  */
-Camera::Camera(glm::vec3 const &position, int width, int height, float fov) :
-position_{position},
-direction_{glm::vec3{0, 0, -1}},
-width_{width},
-height_{height},
-distance_{fov} { // from presentation
+
+Camera::Camera (glm::vec3 const &position, int width, int height, float fov_x) :
+    position_ {position},
+    direction_ {0.0f, 0.0f, -1.0f},
+    width_ {width},
+    height_ {height},
+    distance_ {fov_x}
+{
     distance_ *= MATH_PI / 360.0f;
     distance_ = (float) (0.5f / std::tan(distance_)); // from presentation
     
@@ -80,7 +92,8 @@ distance_{fov} { // from presentation
  * @param y pos of the Pixel on the screen 0 to height
  * @return a new Ray with the pos of the Camera and the Direction onto the camera plane in worldspace
  */
-Ray Camera::generate_ray(int x, int y) const {
+
+Ray Camera::generate_ray (int x, int y) const {
     
     
     glm::vec3 right = glm::normalize(glm::cross(this->direction_, up_vector_));// from the left of the camera plane to the right (normalized)
@@ -117,11 +130,21 @@ Ray Camera::generate_ray(int x, int y) const {
  * Giving the camera a position to point at, we can change the direction
  * @param pos to look at
  */
-void Camera::look_at(glm::vec3 const &pos) {
+
+void Camera::look_at (glm::vec3 const &pos) {
     this->direction_ = pos - this->position_;
     this->direction_ = glm::normalize(this->direction_);
 }
 
+
+/**
+* Camera constructor without angle / fov
+* @param position of the camera as vec3
+* @param direction of the camera as vec3
+* @param width of the screen in pixel/ int
+* @param height of the screen in pixel/ int
+* @param distance of the cameraplane from the cameraorigin to the cameraplane
+*/
 
 void Camera::print() const {
     std::cout << "Camera position: x: " << this->position_[0] << " y: " << this->position_[1] << " z: "
@@ -138,15 +161,33 @@ void Camera::print() const {
  * Sets the position of the camera to a new vector
  * @param pos new position of the camera
  */
-void Camera::set_position(glm::vec3 const &pos) {
+
+void Camera::set_position (glm::vec3 const &pos) {
     this->position_ = pos;
 }
 
-void Camera::translate(glm::vec3 const &pos) {
+/**
+* Camera constructor without angle / fov
+* @param position of the camera as vec3
+* @param direction of the camera as vec3
+* @param width of the screen in pixel/ int
+* @param height of the screen in pixel/ int
+* @param distance of the cameraplane from the cameraorigin to the cameraplane
+*/
+
+void Camera::translate (glm::vec3 const &pos) {
     this->position_ += pos;
 }
 
-void Camera::move(Window const &window) {
+/**
+* Camera constructor without angle / fov
+* @param position of the camera as vec3
+* @param direction of the camera as vec3
+* @param width of the screen in pixel/ int
+* @param height of the screen in pixel/ int
+* @param distance of the cameraplane from the cameraorigin to the cameraplane
+*/
+void Camera::move (Window const &window) {
     
     int w = window.get_key(87);
     int s = window.get_key(83);
@@ -172,7 +213,16 @@ void Camera::move(Window const &window) {
     
 }
 
-void Camera::set_direction(Window const &window) {
+/**
+* Camera constructor without angle / fov
+* @param position of the camera as vec3
+* @param direction of the camera as vec3
+* @param width of the screen in pixel/ int
+* @param height of the screen in pixel/ int
+* @param distance of the cameraplane from the cameraorigin to the cameraplane
+*/
+
+void Camera::set_direction (Window const &window) {
     
     glm::vec2 mouse = window.mouse_position();
     if (0 < mouse[0] && mouse[0] < window.window_size()[0] && 0 < mouse[1] && mouse[1] < window.window_size()[1]) {
@@ -200,7 +250,16 @@ float Camera::random_float() const {
     return ((((float) rand()) / (float) (RAND_MAX / 2.0f)) - 1.0f);// range from -range to range
 }
 
-void Camera::set_depth_of_Field(float doF_strength, float focal_length) {
+/**
+* Camera constructor without angle / fov
+* @param position of the camera as vec3
+* @param direction of the camera as vec3
+* @param width of the screen in pixel/ int
+* @param height of the screen in pixel/ int
+* @param distance of the cameraplane from the cameraorigin to the cameraplane
+*/
+
+void Camera::set_depth_of_Field (float doF_strength, float focal_length) {
     
     
     doF_strength_ = focal_length < INFINITY? focal_length/250 : 0.1f;
@@ -209,18 +268,54 @@ void Camera::set_depth_of_Field(float doF_strength, float focal_length) {
     
 }
 
-glm::vec3 Camera::get_position()const {
+/**
+* Camera constructor without angle / fov
+* @param position of the camera as vec3
+* @param direction of the camera as vec3
+* @param width of the screen in pixel/ int
+* @param height of the screen in pixel/ int
+* @param distance of the cameraplane from the cameraorigin to the cameraplane
+*/
+
+glm::vec3 Camera::get_position() const {
     return position_;
 }
 
-void Camera::set_width_height(int width, int height) {
+/**
+* Camera constructor without angle / fov
+* @param position of the camera as vec3
+* @param direction of the camera as vec3
+* @param width of the screen in pixel/ int
+* @param height of the screen in pixel/ int
+* @param distance of the cameraplane from the cameraorigin to the cameraplane
+*/
+
+void Camera::set_width_height (int width, int height) {
     width_ = width;
     height_ = height;
 }
 
+/**
+* Camera constructor without angle / fov
+* @param position of the camera as vec3
+* @param direction of the camera as vec3
+* @param width of the screen in pixel/ int
+* @param height of the screen in pixel/ int
+* @param distance of the cameraplane from the cameraorigin to the cameraplane
+*/
+
 std::string Camera::get_name() const {
     return name_;
 }
+
+/**
+* Camera constructor without angle / fov
+* @param position of the camera as vec3
+* @param direction of the camera as vec3
+* @param width of the screen in pixel/ int
+* @param height of the screen in pixel/ int
+* @param distance of the cameraplane from the cameraorigin to the cameraplane
+*/
 
 std::string Camera::get_information() const {
     std::string information = " " + name_ + " " + std::to_string(fov_x_) + " " + std::to_string(position_[0]) + " " + std::to_string(position_[1]) + " " + std::to_string(position_[2]) + " " + std::to_string(direction_[0]) + " " + std::to_string(direction_[1]) + " " + std::to_string(direction_[2]) + " " + std::to_string(up_vector_[0]) + " " + std::to_string(up_vector_[1]) + " " + std::to_string(up_vector_[2]);
